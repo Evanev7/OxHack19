9cb255ae72876415196ac7cf6cfd9da7ff523118
Evan <evanev7@gmail.com>
2019-11-16 17:15:56 +0000

    Cursor example

---
+++ cursor example.py
@@ -0,0 +1,7 @@
+import win32api
+from time import sleep
+import numpy as np
+
+for i in np.linspace(0,2*np.pi,1000):
+    win32api.SetCursorPos((960+int(np.floor(200*np.cos(i))),540+int(np.floor(200*np.sin(i)))))
+    sleep(0.0005)
